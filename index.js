81b9fd9bc58841c285c35408dac3dec82ab10e24
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-15 15:21:14 +0100

    second function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function walkDog() {
-    
+    setTimeout(() => {
+        console.log('YOU WALK THE DOG');
+        
+    },1500)
+}
+function cleanKitchen() {
+    setTimeout(() => {
+        console.log('YOU clean the kitchen');
+        
+    },2500)
 }
