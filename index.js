61d6f0e7c18b7c5ddeb7dc9dcfb25eeaa70bc44a
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-15 15:24:09 +0100

    callback chain

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-function walkDog() {
+function walkDog(callback) {
     setTimeout(() => {
         console.log('YOU WALK THE DOG');
-        
+        callback();
     },1500)
 }
-function cleanKitchen() {
+function cleanKitchen(callback) {
     setTimeout(() => {
         console.log('YOU clean the kitchen');
-        
+        callback();
     },2500)
 }
-function takeOutTrash() {
+function takeOutTrash(callback) {
     setTimeout(() => {
         console.log('YOU take out the trash');
-        
+        callback();
     },500)
 }
+
+walkDog(() => {
+    cleanKitchen(() =>{
+        takeOutTrash(() => {
+            console.log('you finish');
+            
+        })
+    })
+})
